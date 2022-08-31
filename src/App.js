ff1fad591e64c8b83ab06a637e4d53c2089f264e
DilanAtomica <dilan-shwane@hotmail.com>
2022-08-31 10:38:16 +0200

    Prepared the App.js file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
+import {createContext, useEffect, useState} from "react";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+
+export const AppContext = createContext();
 
 function App() {
-  return (
-    <div className="App">
 
-    </div>
+
+
+  return (
+      <AppContext.Provider value={}>
+        <div className="App">
+            <BrowserRouter>
+            <Routes>
+              <Route path="/login" element={<> </>} />
+            </Routes>
+          </BrowserRouter>
+        </div>
+      </AppContext.Provider>
   );
 }
 
