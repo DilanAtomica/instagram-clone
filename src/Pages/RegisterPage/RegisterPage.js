153ba3d9aa90c3c4f46ccd9afe0a43d04ace4ed4
DilanAtomica <dilan-shwane@hotmail.com>
2022-08-31 15:07:19 +0200

    Added form validation with yup and react-hook-form

--- src/Pages/RegisterPage/RegisterPage.js
+++ src/Pages/RegisterPage/RegisterPage.js
@@ -1,17 +1,41 @@
 import React from 'react';
 import "./RegisterPage.css";
 import Logo from "../../Images/logo.png";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import {useForm} from "react-hook-form";
+
+const schema = yup.object().shape( {
+    username: yup.string().required("Username is required"),
+    email: yup.string().email().required("Email is required"),
+    password: yup.string().required("Password is required").min(6).max(54),
+    confirmPassword: yup.string().required("Confirm password is required").
+    oneOf([yup.ref("password")], "Password must and should match"),
+}).required();
 
 function RegisterPage(props) {
+
+    const {register, handleSubmit, formState: {errors}} = useForm({
+        resolver: yupResolver(schema),
+    });
+
+    const submitForm = (data) => {
+
+    }
+
     return (
         <div className="registerPage">
             <div className="registerContainer">
                 <img alt="logo" src={Logo} />
-                <form>
-                    <input type="email" placeholder="Email" />
-                    <input type="text" placeholder="Username" />
-                    <input type="password" placeholder="Password" />
-                    <input type="password" placeholder="Confirm Password" />
+                <form onSubmit={handleSubmit(submitForm)}>
+                    <input {...register("email")} type="email" placeholder="Email" />
+                    <p>{errors.email?.message}</p>
+                    <input {...register("username")} type="text" placeholder="Username" />
+                    <p>{errors.username?.message}</p>
+                    <input {...register("password")} type="password" placeholder="Password" />
+                    <p>{errors.password?.message}</p>
+                    <input {...register("confirmPassword")} type="password" placeholder="Confirm Password" />
+                    <p>{errors.confirmPassword?.message}</p>
                     <button type="submit">Register</button>
                 </form>
             </div>
